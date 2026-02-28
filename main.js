2bc6c0ee740c47123f2e82e998fac8b90025217f
MarcoZ23 <zollomarco23@gmail.com>
2026-02-28 13:17:32 +0100

    complete task

--- main.js
+++ main.js
@@ -15,18 +15,23 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 
 // scrivere for da 1 a 100 
 for (let i = 1; i <= 100; i++) {
-     console.log(i);
-// fare un if per i multripli di 3
-    if (i%3==0) {
+// if per i multipli di 3 e 5 
+    if (i%3==0 && i%5==0) {
+    console.log("FizzBuzz");
+        
+    }
+// else if per i multripli di 3
+    else if (i%3==0) {
     console.log("Fizz");
     }
 // else if per i multipli di 5 
     else if (i%5==0) {
-        console.log("Buzz");
+    console.log("Buzz");
         
+    }
+    else {console.log(i);
     }
 }
    
     
 
-// else if per i multipli di 3 e 5 
