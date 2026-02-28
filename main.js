494177e67ddeb972e808f4e29a703db41554b640
MarcoZ23 <zollomarco23@gmail.com>
2026-02-28 12:56:53 +0100

    add first multiple

--- main.js
+++ main.js
@@ -14,13 +14,19 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 */
 
 // scrivere for da 1 a 100 
-for (let i = 1; i <= 100; i++)
-    console.log(i);
-    
-
-
-// fare un if per i multripli di 3 
-
+for (let i = 1; i <= 100; i++) {
+     console.log(i);
+// fare un if per i multripli di 3
+    if (i%3==0) {
+    console.log("Fizz");
+    }
 // else if per i multipli di 5 
+    else if (i%5==0) {
+        console.log("Buzz");
+        
+    }
+}
+   
+    
 
 // else if per i multipli di 3 e 5 
