e9adde987265f4ca1f3d0010d5baf722e38f61fc
MarcoZ23 <zollomarco23@gmail.com>
2026-03-02 20:55:27 +0100

    correct indentation

--- main.js
+++ main.js
@@ -16,17 +16,17 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 // scrivere for da 1 a 100 
 for (let i = 1; i <= 100; i++) {
 // if per i multipli di 3 e 5 
-    if (i%3==0 && i%5==0) {
-    console.log("FizzBuzz");
+    if (i %3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
         
     }
 // else if per i multripli di 3
-    else if (i%3==0) {
-    console.log("Fizz");
+    else if (i % 3 == 0) {
+        console.log("Fizz");
     }
 // else if per i multipli di 5 
-    else if (i%5==0) {
-    console.log("Buzz");
+    else if (i % 5 == 0) {
+        console.log("Buzz");
         
     }
     else {console.log(i);
